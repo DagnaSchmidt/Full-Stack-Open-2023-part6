3663fcce6fffe27658a1d21fceabd6b36e46222f
Dagna Schmidt <dagdagi889@gmail.com>
2023-06-09 09:52:21 +0200

    unicafe redux start files

---
+++ src/unicafe-redux/App.js
@@ -0,0 +1,37 @@
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+
+import { initialState } from './reducer';
+import counterReducer from './reducer';
+
+const store = createStore(
+    reducer,
+    initialState
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+
+function App() {
+
+    const good = () => {
+        store.dispatch({
+          type: 'GOOD'
+        })
+      };
+
+    return (
+        <Provider store={store}>
+            <div>
+                <button onClick={good}>good</button> 
+                <button>ok</button> 
+                <button>bad</button>
+                <button>reset stats</button>
+                <div>good {store.getState().good}</div>
+                <div>ok</div>
+                <div>bad</div>
+            </div>
+      </Provider>
+    );
+  }
+  
+  export default App;
