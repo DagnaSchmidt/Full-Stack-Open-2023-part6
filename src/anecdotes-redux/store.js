d77666c05fa45f94784eeb05fd955c3a9f7811db
Dagna Schmidt <dagdagi889@gmail.com>
2023-06-11 14:48:57 +0200

    part6 6.14

--- src/anecdotes-redux/store.js
+++ src/anecdotes-redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import anecdotesReducer from './reducers/anecdotesReducer';
+import anecdotesReducer, {setAnecdotes} from './reducers/anecdotesReducer';
 import filterReducer from './reducers/filterReducer';
 import notificationReducer from './reducers/notificationReducer';
+import { getAll } from './services/anecdotes';
 
 export const store = configureStore({
     reducer: {
@@ -9,4 +10,8 @@ export const store = configureStore({
         filter: filterReducer,
         notification: notificationReducer
     }
+});
+
+getAll().then(a => {
+    store.dispatch(setAnecdotes(a));
 });
